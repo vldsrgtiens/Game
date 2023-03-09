13bf76586892a04bb266712c46ca5a63f147b029
vldsrg.tiens@yandex.ru <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>
2023-03-09 17:56:14 +0300

    COMMIT

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class GlobalVariables : MonoBehaviour
 {
     public enum  TypePerson { Hero, Robot, Enemy };
-    public enum  MotionStatus { isWaiting, isMoving, isRotating, isBeforeTargetPosition,isBeforeTargetRotation };
-    public enum TypeDirection { North=0, NorthEast=1, SouthEast=2, South=3, SouthWest=4, NorthWest=5, NorthNorth=6}
-    public static float globVal = 5f;
+    public enum  MotionStatus { IsWaiting, IsMoving, IsRotating, IsBeforeTargetPosition, IsBeforeTargetRotation };
     public static float angleRotate = 60f;
-    const int scaleTerrain = 2;
+
     
    
 
--- Assets/Scripts/MotionModule.cs
+++ Assets/Scripts/MotionModule.cs
@@ -1,19 +1,14 @@
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 //using Vector3 = UnityEngine.Vector3;
 
 public class MotionModule : MonoBehaviour
 {
-    //public GlobalVariables.TypeOrientation mmOrientation;
-    
-
-    public GlobalVariables.MotionStatus mmStatus = GlobalVariables.MotionStatus.isWaiting;
-
+    public GlobalVariables.MotionStatus mmStatus = GlobalVariables.MotionStatus.IsWaiting;
     public Vector3 mmTargetPosition = Vector3.zero;
     public Compass.TypeDirection mmTargetDirection;
-    
-    
 
     private float mmSpeed;
     private float directionOfRotation = 1f;
@@ -23,27 +18,21 @@ public class MotionModule : MonoBehaviour
     Transform mm_Transform;
     ObjectItem mm_ObjectItem;
 
-
-
     void Awake()
     {
-        //mmDirection = GlobalVariables.TypeSideWorld[3];
-        //transform.localEulerAngles.y / GlobalVariables.angleRotate;
-        
         mm_Rigidbody = GetComponent<Rigidbody>();
-        print("transform position = "+transform.position);
         mm_ObjectItem = GetComponent<ObjectItem>();
 
         mmSpeed = mm_ObjectItem.speed;
 
-        mmStatus = GlobalVariables.MotionStatus.isMoving;
+        //RotateTo(Compass.TypeDirection.SouthEast);
         
         Debug.Log("MotionDrive Awake() was called");
     }
     
     void FixedUpdate()
     {
-        if (mmStatus == GlobalVariables.MotionStatus.isMoving )
+        if (mmStatus == GlobalVariables.MotionStatus.IsMoving )
         {
             // вектор направления к цели. вектор надо нормализовать, чтобы скорость была постоянной, иначе она будет зависеть от расстояния.
             Vector3 heading = (mmTargetPosition - transform.position).normalized;
@@ -53,50 +42,64 @@ public class MotionModule : MonoBehaviour
             
             if (dist_toTarget < step) 
             {
-                print("attention: near the target "+dist_toTarget);
-                mmStatus = GlobalVariables.MotionStatus.isBeforeTargetPosition;
+                mmStatus = GlobalVariables.MotionStatus.IsBeforeTargetPosition;
             }
-            print("position: "+transform.position);
+            //print("position: "+transform.position);
             mm_Rigidbody.MovePosition(transform.position + heading * step);
         }
+        
+        //==============================
 
-        if (mmStatus == GlobalVariables.MotionStatus.isBeforeTargetPosition)
+        if (mmStatus == GlobalVariables.MotionStatus.IsBeforeTargetPosition)
         {
             mm_Rigidbody.position = mmTargetPosition;
             mm_Rigidbody.velocity = Vector3.zero; 
-            mmStatus = GlobalVariables.MotionStatus.isWaiting;
-            
-            print("attention: the target ");
+            mmStatus = GlobalVariables.MotionStatus.IsWaiting;
         } 
         
-        if (mmStatus == GlobalVariables.MotionStatus.isWaiting)
+        //==============================
+        
+        if (mmStatus == GlobalVariables.MotionStatus.IsWaiting)
         {
-            mmTargetPosition=transform.position+(2*(new Vector3(40f,0f,0f)-transform.position));
-            mmStatus = GlobalVariables.MotionStatus.isMoving;
+            //mmTargetPosition=transform.position+(2*(new Vector3(40f,0f,0f)-transform.position));
+            //mmStatus = GlobalVariables.MotionStatus.isMoving;
         }
         
-        if (mmStatus == GlobalVariables.MotionStatus.isRotating )
+        //==============================
+        
+        if (mmStatus == GlobalVariables.MotionStatus.IsRotating )
         {
             //mmTargeDirectionV3 = Compass.DirectionToVector3(mmTargetDirection);
             
-            float _delta = (60f * (int)mmTargetDirection) - oldRotation ;
-            print("delta Rotation = "+_delta);
+            float _delta = MathF.Abs((GlobalVariables.angleRotate * (int)mmTargetDirection) - oldRotation) ;
 
+            float _rot = (mmSpeed / 5) * directionOfRotation;
             
-            if (_delta < 1f) 
+            if (_delta < 2.0f) 
             {
-                print("attention: near the target "+_delta);
-                mmStatus = GlobalVariables.MotionStatus.isBeforeTargetRotation;
+                mmStatus = GlobalVariables.MotionStatus.IsBeforeTargetRotation;
             }
-            print("rotationn: "+transform.rotation);
-            mm_Transform.Rotate(new Vector3(0f,mmSpeed*directionOfRotation,0f));
+
+            transform.Rotate(0f,_rot,0f);
+            oldRotation = transform.eulerAngles.y;
+        }
+        
+        //==============================
+
+        if (mmStatus == GlobalVariables.MotionStatus.IsBeforeTargetRotation)
+        {
+            Vector3 rotate = transform.eulerAngles;
+            rotate.y = 60.0f * (int)mmTargetDirection;
+            transform.rotation = Quaternion.Euler(rotate);
+            mm_ObjectItem.direction = mmTargetDirection;
+            mmStatus = GlobalVariables.MotionStatus.IsWaiting;
         }
     }
 
     public void MoveForward(Vector3 target)
     {
         mmTargetPosition = target;
-        mmStatus=GlobalVariables.MotionStatus.isMoving;
+        mmStatus=GlobalVariables.MotionStatus.IsMoving;
     }
         
     public void MoveBack(Rigidbody rb,Vector3 vector3To, float speed)
@@ -109,16 +112,30 @@ public class MotionModule : MonoBehaviour
     {
         //player.transform.RotateAround(player.transform.position,player.transform.up,_rotation_y*Time.deltaTime);
         mmTargetDirection = Compass.RotateToLeft(mm_ObjectItem.direction);
-        oldRotation = 60f * (int)mm_ObjectItem.direction;
+        oldRotation = GlobalVariables.angleRotate * (int)mm_ObjectItem.direction;
         directionOfRotation = -1f;
-        mmStatus = GlobalVariables.MotionStatus.isRotating;
+        mmStatus = GlobalVariables.MotionStatus.IsRotating;
     }
     public void RotateToRight()
     {
         //player.transform.RotateAround(player.transform.position,player.transform.up,_rotation_y*Time.deltaTime);
-        mmTargetDirection = Compass.RotateToLeft(mm_ObjectItem.direction);
-        oldRotation = 60f * (int)mm_ObjectItem.direction;
+        mmTargetDirection = Compass.RotateToRight(mm_ObjectItem.direction);
+        oldRotation = GlobalVariables.angleRotate * (int)mm_ObjectItem.direction;
         directionOfRotation = 1f;
-        mmStatus = GlobalVariables.MotionStatus.isRotating;
+        mmStatus = GlobalVariables.MotionStatus.IsRotating;
+    }
+    
+    public void RotateTo(Compass.TypeDirection target)
+    {
+        //player.transform.RotateAround(player.transform.position,player.transform.up,_rotation_y*Time.deltaTime);
+        mmTargetDirection = target;
+        oldRotation = GlobalVariables.angleRotate * (int)mm_ObjectItem.direction;
+        float t1 = Mathf.Abs(((int)mmTargetDirection * GlobalVariables.angleRotate) - (oldRotation));
+        float t2 = Mathf.Abs((int)mmTargetDirection * GlobalVariables.angleRotate - (360f + oldRotation));
+        if (t1 < t2)
+            directionOfRotation = 1f;
+        else
+            directionOfRotation = -1f;
+        mmStatus = GlobalVariables.MotionStatus.IsRotating;
     }
 }
