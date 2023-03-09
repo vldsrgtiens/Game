eb4027ce11f7af4c8cb2f4162ad3be52606f2837
vldsrg.tiens@yandex.ru <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>
2023-03-09 22:25:49 +0300

    commit

---
+++ Assets/Scripts/MotionDriver.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionDriver : MonoBehaviour
+{
+    public GameObject obj;
+    public GameObject mainCamera;//MainCamera
+    public GameObject eye;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SetCameraToHero();
+    }
+
+    void SetCameraToHero()
+    {
+        
+        if (this.tag == "Hero")
+        {
+            mainCamera = GameObject.Find("Main Camera");
+            
+            
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.tag == "eye")
+                {
+                    eye = child.gameObject;
+                }
+            }
+            if (eye != null)
+            {
+                if (mainCamera != null)
+                {
+                    print("camera found");
+                    mainCamera.transform.SetParent(eye.transform, false);
+                    mainCamera.transform.localPosition = Vector3.zero;
+                    print("set parent camera");
+                }
+                else print("ATTANTION: camera not found");
+            }
+            else
+            {
+                print("ATTANTION: eye not found");
+            }
+        }
+    }
+}
